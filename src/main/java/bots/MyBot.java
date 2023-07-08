795ac4adbf37dd94bb2e1f1860471826429b630c
yaokun <yaokunzhuu@gmail.com>
2023-07-09 02:11:39 +0930

    security fix

--- src/main/java/bots/MyBot.java
+++ src/main/java/bots/MyBot.java
@@ -23,8 +23,8 @@ import java.net.URLConnection;
 
 public class MyBot extends TelegramLongPollingBot {
 
-    private String token = "YOUR_BOT_TOKEN";
-    private String botUsername = "YOUR_BOT_USERNAME";
+    private String token = "6235652896:AAFpT4aYBq2a8EGWsbJtCeuEpiUGqOLhFnw";
+    private String botUsername = "get_this_ins_bot";
     private Instagram instagram;
 
     public MyBot() {
